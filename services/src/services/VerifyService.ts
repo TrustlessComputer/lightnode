60787b94e73d2d3ef20350379c85ccad1d6f0bab
zkryp <8017@newbitcoincity.com>
2024-06-06 13:16:29 +0000

    update

--- services/src/services/VerifyService.ts
+++ services/src/services/VerifyService.ts
@@ -1,14 +1,24 @@
 import { exec } from 'child_process';
+import { time } from 'console';
 
 export const startVerifyService = (cmd: string) => {
-  console.log('Starting verify Supersonic service...');
+  console.log(`[${new Date().toTimeString()}] Start verifying service ...`);
 
   // Example command to run (replace with your actual command)
   const command = cmd;
+  let isRunning = false;
 
   // Function to execute the command
   const runCommand = () => {
+    if (isRunning) {
+      console.log("Previous command is still running, skipping this execution.");
+      return;
+    }
+    
+    isRunning = true;
+
     exec(command, (error, stdout, stderr) => {
+      isRunning = false;
       if (error) {
         console.error(`Error executing command: ${error.message}`);
         return;
@@ -19,8 +29,7 @@ export const startVerifyService = (cmd: string) => {
 
   // Run the command immediately
   runCommand();
-  console.log("[Verify Service] Command executed successfully")
 
   // Schedule the command to run every 10 minutes
-  setInterval(runCommand, 600000);
+  setInterval(runCommand, 60000);
 };
