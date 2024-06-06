4e1449d8d573922952070feacf0bc216323b41bf
zkryp <8017@newbitcoincity.com>
2024-06-06 12:50:58 +0000

    add config

--- services/src/index.ts
+++ services/src/index.ts
@@ -3,24 +3,20 @@ import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import { startVerifyService } from './services/VerifyService';
-
+const cors = require('cors');
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
+
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
+app.use(cors());
 
 // Use routes
 app.use('/api', routes);
-app.use('/status', routes);
 
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
